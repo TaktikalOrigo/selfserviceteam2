291f06835f791d36a91fde4c0f192b4e99f15b28
alexharri <alex@taktikal.is>
2020-02-12 13:16:38 +0000

    add maternity leave fields

--- src/pages/umsokn.tsx
+++ src/pages/umsokn.tsx
@@ -6,6 +6,19 @@ export default () => {
     <StepManager
       initialFields={{
         ssn: "",
+        name: "",
+        email: "",
+        phoneNumber: "",
+        bankNumber: "",
+        expectedDateOfBirth: "",
+        union: "",
+        pensionFund: "",
+        personalFundContribution: 0,
+        personalTaxCreditUsagePercent: 100,
+        monthsOfLeaveAvailable: 0,
+        timePeriods: [],
+        hasEmployeeAccepted: false,
+        hasGovernmentAccepted: false,
       }}
       layoutComponent={props => <div style={{ padding: 32 }}>{props.children}</div>}
       steps={maternityLeaveSteps}
