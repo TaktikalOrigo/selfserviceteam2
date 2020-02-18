3ffbeb4a29f0bd6a84b09a8bd2094d5f01c73106
alexharri <alex@taktikal.is>
2020-02-18 12:10:57 +0000

    add transition to mobile header background and increase the opacity of it

--- src/client/components/header/MobileHeader.styles.ts
+++ src/client/components/header/MobileHeader.styles.ts
@@ -1,135 +1,149 @@
 import { StyleParams } from "@taktikal/stylesheets";
 import { cssVariables } from "~/cssVariables";
 
-export default ({ css }: StyleParams) => ({
-  header: css`
-    height: ${cssVariables.headerHeight};
-  `,
+export default ({ css, keyframes }: StyleParams) => {
+  const bgFadeIn = keyframes`
+    from { opacity: 0; }
+    to { opacity: 1; }
+  `;
 
-  header__content: css`
-    height: ${cssVariables.headerHeight};
-    margin: 0 auto;
-    padding: 0 32px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  `,
+  const bgFadeOut = keyframes`
+    from { opacity: 1; transform: none; }
+    to { opacity: 0; transform: none; }
+  `;
 
-  modal: css`
-    padding: 80px 24px 0;
-    max-width: 80vw;
-    width: 320px;
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    background: white;
-    box-shadow: 0 0 32px rgba(0, 0, 0, 0.1);
-    transform: translateX(100%);
-    transition: transform 0.3s;
-    z-index: 150;
+  return {
+    header: css`
+      height: ${cssVariables.headerHeight};
+    `,
 
-    &--open {
-      transform: translateX(0);
-    }
-  `,
+    header__content: css`
+      height: ${cssVariables.headerHeight};
+      margin: 0 auto;
+      padding: 0 32px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    `,
 
-  background: css`
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: rgba(0, 0, 0, 0.1);
-    display: none;
-    z-index: 100;
+    modal: css`
+      padding: 80px 24px 0;
+      max-width: 80vw;
+      width: 320px;
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      background: white;
+      box-shadow: 0 0 32px rgba(0, 0, 0, 0.1);
+      transform: translateX(100%);
+      transition: transform 0.3s;
+      z-index: 150;
 
-    &--open {
-      display: block;
-    }
-  `,
+      &--open {
+        transform: translateX(0);
+      }
+    `,
 
-  logo: css`
-    svg {
-      height: 24px;
-      width: 148px;
-    }
-  `,
+    background: css`
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: rgba(0, 0, 0, 0.3);
+      z-index: 100;
+      animation: ${bgFadeOut} 0.3s;
+      transform: translateX(100%);
 
-  burger: css`
-    position: fixed;
-    width: 32px;
-    height: 32px;
-    outline: none;
-    border: none;
-    background: transparent;
-    z-index: 200;
-    top: 24px;
-    right: 24px;
-    cursor: pointer;
-  `,
+      &--open {
+        animation: ${bgFadeIn} 0.3s;
+        transform: none;
+      }
+    `,
 
-  burger__line: css`
-    height: 2px;
-    position: absolute;
-    left: 4px;
-    right: 4px;
-    background: ${cssVariables.colorBlack};
-    transition: transform 0.3s;
+    logo: css`
+      svg {
+        height: 24px;
+        width: 148px;
+      }
+    `,
 
-    &--0 {
-      top: 6px;
-    }
-    &--1 {
-      top: 50%;
-      transform: translateY(-50%);
-    }
-    &--2 {
-      bottom: 6px;
-    }
+    burger: css`
+      position: fixed;
+      width: 32px;
+      height: 32px;
+      outline: none;
+      border: none;
+      background: transparent;
+      z-index: 200;
+      top: 24px;
+      right: 24px;
+      cursor: pointer;
+    `,
 
-    &--0.open {
-      transform: translateY(9px) rotate(45deg);
-    }
-    &--1.open {
-      transform: translateY(-50%) scaleX(0);
-    }
-    &--2.open {
-      transform: translateY(-9px) rotate(-45deg);
-    }
-  `,
+    burger__line: css`
+      height: 2px;
+      position: absolute;
+      left: 4px;
+      right: 4px;
+      background: ${cssVariables.colorBlack};
+      transition: transform 0.3s;
 
-  container: css`
-    text-align: right;
-  `,
+      &--0 {
+        top: 6px;
+      }
+      &--1 {
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      &--2 {
+        bottom: 6px;
+      }
 
-  name: css`
-    font-family: ${cssVariables.bodyFont};
-    font-size: 18px;
-    font-weight: 600;
-    color: ${cssVariables.colorBlack};
-    line-height: 1;
-    margin-bottom: 8px;
-  `,
+      &--0.open {
+        transform: translateY(9px) rotate(45deg);
+      }
+      &--1.open {
+        transform: translateY(-50%) scaleX(0);
+      }
+      &--2.open {
+        transform: translateY(-9px) rotate(-45deg);
+      }
+    `,
 
-  ssn: css`
-    font-family: ${cssVariables.bodyFont};
-    font-size: 12px;
-    color: ${cssVariables.colorBlack};
-    line-height: 1;
-  `,
+    container: css`
+      text-align: right;
+    `,
 
-  logoutButton: css`
-    color: ${cssVariables.colorPrimary};
-    font-family: ${cssVariables.bodyFont};
-    font-weight: 600;
-    display: inline-block;
-    margin-top: 24px;
-    padding: 0 24px;
-    line-height: 32px;
-    font-size: 16px;
-    background: ${cssVariables.colorOffWhite};
-    border: none;
-    border-radius: 4px;
-  `,
-});
+    name: css`
+      font-family: ${cssVariables.bodyFont};
+      font-size: 18px;
+      font-weight: 600;
+      color: ${cssVariables.colorBlack};
+      line-height: 1;
+      margin-bottom: 8px;
+    `,
+
+    ssn: css`
+      font-family: ${cssVariables.bodyFont};
+      font-size: 12px;
+      color: ${cssVariables.colorBlack};
+      line-height: 1;
+    `,
+
+    logoutButton: css`
+      color: ${cssVariables.colorPrimary};
+      font-family: ${cssVariables.bodyFont};
+      font-weight: 600;
+      display: inline-block;
+      margin-top: 24px;
+      padding: 0 24px;
+      line-height: 32px;
+      font-size: 16px;
+      background: ${cssVariables.colorOffWhite};
+      border: none;
+      border-radius: 4px;
+    `,
+  };
+};
